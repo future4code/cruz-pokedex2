9c372b806d2ddd7dd07ecb2eefd0e12c15e07a45
KeLvIA KeLlInE <kelviaks@gmail.com>
2021-04-19 15:21:27 -0300

    Criado component dos cards, constante url e página HomePage

---
+++ pokedex/src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PokeCard from '../components/cards/Cards';
+
+const HomePage  = (props) => {
+  return(
+    <div>
+      <PokeCard/>
+    </div>
+  )
+}
+export default HomePage;
