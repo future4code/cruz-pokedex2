4a192e99a3419304595d3dce0a9bfef68268e973
KeLvIA KeLlInE <kelviaks@gmail.com>
2021-04-19 16:00:27 -0300

    transformando o título do primeiro button em props

--- pokedex/src/components/cards/Cards.js
+++ pokedex/src/components/cards/Cards.js
@@ -7,7 +7,7 @@ const PokeCard = (props) => {
       <PokeImgs alt={props.poke.name} src={props.poke.sprites.front_default} />
       {/* na API a foto do pokemon, de frente, está dentro de Sprites{front_default} */}
       <ButtonContainer>
-        <button>Adicionar a Pokédex </button>
+        <button>{props.title}</button>
         <button>Ver Detalhes</button>
       </ButtonContainer>
     </CardContainer>
