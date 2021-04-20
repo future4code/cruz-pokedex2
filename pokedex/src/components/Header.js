27f33e0e0b08331b2f4fcf90dff2aef5116de82f
samenc99 <mateus.enc@gmail.com>
2021-04-20 13:44:48 -0300

    alteração do header

--- pokedex/src/components/Header.js
+++ pokedex/src/components/Header.js
@@ -2,18 +2,24 @@ import React from 'react';
 import styled from 'styled-components';
 
 const HeaderContainer = styled.div`
-  height: 10vh;
+  width: 100%;
+  height: 60px;
   background: #F0FFFF;
-  position: relative;
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
   color: black;
-` 
+`
+
 const Header = (props) => {
   return(
     <HeaderContainer>
+      <button onClick={props.onClickbutton}>{props.buttonTitle}</button>
       <h1>{props.title}</h1>
+      {
+        props.details &&
+          <button onClick={props.idPokemon}>Adicionar/Remover da pokedex</button>
+      }
     </HeaderContainer>
   )
 }
