4dabe88bcf2d1a818988a0cf10d827fb6437e8e5
samenc99 <mateus.enc@gmail.com>
2021-04-25 23:06:49 -0300

    correção bug quando acessa direto pela url

--- pokedex/src/pages/battle/BattlePage.js
+++ pokedex/src/pages/battle/BattlePage.js
@@ -1,4 +1,4 @@
-import React,{useContext, useEffect} from 'react'
+import React,{useContext, useEffect, useState} from 'react'
 import ContextPokemons from "../../contexts/contexts";
 import Header from "../../components/header/Header";
 import styled from 'styled-components'
@@ -8,14 +8,30 @@ const Container = styled.div`
   height: 100vh;
   width: 100%;
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  align-items: center;  
+`
+const DivPokes = styled.div`
+  display: flex;
+  margin-top:100px;
+`
+const Vencedor = styled.h1`
+  
 `
 
 export default function BattlePage(){
   const {states} = useContext(ContextPokemons)
   const history = useHistory()
-  const {battle} = states
-  const [poke1,poke2] = battle
+  let poke1 = {}
+  let poke2 = {}
+
+  if(states.battle.length===2){
+    const {battle} = states
+    poke1 = battle[0]
+    poke2 = battle[1]
+  }else{
+    history.push('/')
+  }
 
   const valor=(poke,name)=>{
     return Number(poke[name])
@@ -34,21 +50,31 @@ export default function BattlePage(){
     const poke1Soma = soma(poke1)
     const poke2Soma = soma(poke2)
     if(poke1Soma>poke2Soma){
-      return <div>Poke1 vencedor</div>
+      return <Vencedor>{poke1.name.toUpperCase()} é vencedor</Vencedor>
     }
     else if(poke1Soma<poke2Soma){
-      return <div>Poke2 vencedor</div>
+      return <Vencedor>{poke2.name.toUpperCase()} é vencedor</Vencedor>
     }
     else{
-      return <div>Empate</div>
+      return <Vencedor>Empate</Vencedor>
     }
   }
 
   return(
     <Container>
-      <PokeCard key={1} poke={poke1} title={"Adicionar na pokedex"} />
-      <PokeCard key={2} poke={poke2} title={"Adicionar na pokedex"} />
-      {vencedor()}
+      { poke1.name && poke2.name && <>
+        <Header
+          title={`${poke1.name.toUpperCase()} vs ${poke2.name.toUpperCase()}`}
+          goTo={() => history.goBack()}
+          buttonTitle={"Voltar"}
+        />
+        <DivPokes>
+        <PokeCard key={1} poke={poke1} title={"Adicionar na pokedex"} />
+        <PokeCard key={2} poke={poke2} title={"Adicionar na pokedex"} />
+        </DivPokes>
+        {vencedor()}
+      </>
+      }
     </Container>
   )
 }
